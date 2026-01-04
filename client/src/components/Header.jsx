4090ccb4440dd0cc8d9f565d502e2d828d86e631
Lahiru Niroshan <e20272@eng.pdn.ac.lk>
2026-01-04 12:09:12 +0530

    create the header section

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,46 @@
+import { Button, Navbar, TextInput, NavbarCollapse, NavbarLink, NavbarToggle } from "flowbite-react";
+import { Link, useLocation } from "react-router-dom";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
+
+
+export default function Header() {
+  const path = useLocation().pathname;
+  return (
+    <Navbar className='border-b-2'>
+      <Link to="/" className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
+        <span className="px-2 py-1 text-white rounded-lg bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+          Lahiru's
+        </span>
+        Blog
+      </Link>
+      <form>
+        <TextInput 
+          type="text"
+          placeholder="Search..."
+          rightIcon={AiOutlineSearch}
+          className="hidden lg:inline"
+        />
+      </form>
+      <Button className="w-18 h-10 lg:hidden" color= 'light' pill>
+        <AiOutlineSearch />
+      </Button>
+      <div className="flex gap-2 md:order-2">
+        <Button className="w-12 h-10 hidden sm:inline" color= 'light' pill>
+          <FaMoon />
+        </Button>
+        <Link to="/sign-in">
+          <Button className="bg-gradient-to-br from-purple-600 to-blue-500 text-white hover:bg-gradient-to-bl focus:ring-blue-300 dark:focus:ring-blue-800" >
+            Sign In
+          </Button>
+          <NavbarToggle />
+        </Link>
+      </div>
+      <NavbarCollapse>
+        <NavbarLink active={path === '/'} as={'div'}><Link to='/'>Home</Link></NavbarLink>
+        <NavbarLink active={path === '/about'} as={'div'}><Link to='/about'>About</Link></NavbarLink>
+        <NavbarLink active={path === '/projects'} as={'div'}><Link to='/projects'>Projects</Link></NavbarLink>
+      </NavbarCollapse>
+    </Navbar>
+  )
+}
