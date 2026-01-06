bd19b669cec0e008dfbc3e05b949f8c1b78fb69b
Lahiru Niroshan <e20272@eng.pdn.ac.lk>
2026-01-06 21:01:34 +0530

    create the footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,11 +6,13 @@ import Dashboard from "./pages/Dashboard"
 import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 export default function App() {
   return (
     <BrowserRouter>
       <Header />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -19,6 +21,7 @@ export default function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
