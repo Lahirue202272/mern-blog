4af22748ebb417a122b91b901112e52433b6b958
Lahiru Niroshan <e20272@eng.pdn.ac.lk>
2026-01-09 19:31:56 +0530

    add update user api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import { test } from '../controllers/user.controller.js';
+import { test, updateUser } from '../controllers/user.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
@@ -8,5 +9,6 @@ const router = express.Router();
 // });
 
 router.get('/test', test);
+router.put('/update/:userId',verifyToken, updateUser);
 
 export default router;
