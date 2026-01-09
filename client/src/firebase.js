3c4f17a7da8c866e5ab9ec77d1c07014ec0ee1c6
Lahiru Niroshan <e20272@eng.pdn.ac.lk>
2026-01-09 17:14:50 +0530

    complete user image upload functionality

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,17 +1,35 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: "mern-blog-138cd.firebaseapp.com",
+//   projectId: "mern-blog-138cd",
+//   storageBucket: "mern-blog-138cd.firebasestorage.app",
+//   messagingSenderId: "1024647702316",
+//   appId: "1:1024647702316:web:bb0f7df68dce55ff162e89"
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "mern-blog-138cd.firebaseapp.com",
-  projectId: "mern-blog-138cd",
-  storageBucket: "mern-blog-138cd.firebasestorage.app",
-  messagingSenderId: "1024647702316",
-  appId: "1:1024647702316:web:bb0f7df68dce55ff162e89"
+  authDomain: "mern-estate-d2809.firebaseapp.com",
+  projectId: "mern-estate-d2809",
+  storageBucket: "mern-estate-d2809.firebasestorage.app",
+  messagingSenderId: "104287565322",
+  appId: "1:104287565322:web:ad448c74ce61e57f5cd1f3"
 };
 
 // Initialize Firebase
