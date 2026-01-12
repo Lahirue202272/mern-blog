6a921da346dbb7daa36d2b40e3995ca212cfc8a7
Lahiru Niroshan <e20272@eng.pdn.ac.lk>
2026-01-12 16:03:13 +0530

    add show more functionality to the posts results of the dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -7,6 +7,7 @@ export default function DashPosts() {
 
   const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
+  const [showMore, setShowMore] = useState(true);
   console.log(userPosts);
   useEffect(() => {
     const fetchPosts = async () => {
@@ -15,6 +16,9 @@ export default function DashPosts() {
         const data = await res.json();
         if (res.ok) {
           setUserPosts(data.posts);
+          if (data.posts.length < 9) {
+            setShowMore(false);
+          }
         }
       } catch (error) {
         console.log(error.message);
@@ -25,62 +29,90 @@ export default function DashPosts() {
     }
   }, [currentUser._id]);
 
+  const handleShowMore = async () => {
+    const startIndex = userPosts.length;
+    try {
+      const res = await fetch(
+        `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
+      );
+      const data = await res.json();
+      if (res.ok) {
+        setUserPosts((prev) => [...prev, ...data.posts]);
+        if (data.posts.length < 9) {
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className='table-auto overflow-x-auto overflow-y-auto max-h-[70vh] md:mx-auto p-3 scrollbar-thin scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
       {currentUser.isAdmin && userPosts.length > 0 ? (
         <>
           <Table hoverable className='shadow-md'>
             <TableHead>
-              <TableHeadCell>Date updated</TableHeadCell>
-              <TableHeadCell>Post image</TableHeadCell>
-              <TableHeadCell>Post title</TableHeadCell>
-              <TableHeadCell>Category</TableHeadCell>
-              <TableHeadCell>Delete</TableHeadCell>
-              <TableHeadCell>
-                <span>Edit</span>
-              </TableHeadCell>
+              <TableRow>
+                <TableHeadCell>Date updated</TableHeadCell>
+                <TableHeadCell>Post image</TableHeadCell>
+                <TableHeadCell>Post title</TableHeadCell>
+                <TableHeadCell>Category</TableHeadCell>
+                <TableHeadCell>Delete</TableHeadCell>
+                <TableHeadCell>
+                  <span>Edit</span>
+                </TableHeadCell>
+              </TableRow>
             </TableHead>
-            {userPosts.map((post) => (
-              <TableBody className='divide-y'>
-                <TableRow className='bg-white dark:border-gray-700 dark:bg-gray-800'>
-                  <TableCell>
-                    {new Date(post.updatedAt).toLocaleDateString()}
-                  </TableCell>
-                  <TableCell>
-                    <Link to={`/post/${post.slug}`}>
-                      <img
-                        src={post.image}
-                        alt={post.title}
-                        className='w-20 h-10 object-cover bg-gray-500'
-                      />
-                    </Link>
-                  </TableCell>
-                  <TableCell>
-                    <Link
-                      className='font-medium text-gray-900 dark:text-white'
-                      to={`/post/${post.slug}`}
-                    >
-                      {post.title}
-                    </Link>
-                  </TableCell>
-                  <TableCell>{post.category}</TableCell>
-                  <TableCell>
-                    <span className='font-medium text-red-500 hover:underline cursor-pointer'>
-                      Delete
-                    </span>
-                  </TableCell>
-                  <TableCell>
-                    <Link
-                      className='text-teal-500 hover:underline'
-                      to={`/update-post/${post._id}`}
-                    >
-                      <span>Edit</span>
-                    </Link>
-                  </TableCell>
-                </TableRow>
-              </TableBody>
-            ))}
+            <TableBody className='divide-y'>
+              {userPosts.map((post) => (
+                  <TableRow key={post._id}  className='bg-white dark:border-gray-700 dark:bg-gray-800'>
+                    <TableCell>
+                      {new Date(post.updatedAt).toLocaleDateString()}
+                    </TableCell>
+                    <TableCell>
+                      <Link to={`/post/${post.slug}`}>
+                        <img
+                          src={post.image}
+                          alt={post.title}
+                          className='w-20 h-10 object-cover bg-gray-500'
+                        />
+                      </Link>
+                    </TableCell>
+                    <TableCell>
+                      <Link
+                        className='font-medium text-gray-900 dark:text-white'
+                        to={`/post/${post.slug}`}
+                      >
+                        {post.title}
+                      </Link>
+                    </TableCell>
+                    <TableCell>{post.category}</TableCell>
+                    <TableCell>
+                      <span className='font-medium text-red-500 hover:underline cursor-pointer'>
+                        Delete
+                      </span>
+                    </TableCell>
+                    <TableCell>
+                      <Link
+                        className='text-teal-500 hover:underline'
+                        to={`/update-post/${post._id}`}
+                      >
+                        <span>Edit</span>
+                      </Link>
+                    </TableCell>
+                  </TableRow>
+              ))}
+            </TableBody>
           </Table>
+          {showMore && (
+            <button
+              onClick={handleShowMore}
+              className='w-full text-teal-500 self-center text-sm py-7'
+            >
+              Show more
+            </button>
+          )}
         </>
       ) : (
         <p>You have no posts yet!</p>
