cdf830e6098e581363a1debda1705580bbb1509b
Lahiru Niroshan <e20272@eng.pdn.ac.lk>
2026-01-13 09:08:15 +0530

    add delete user functionality to the admin user

--- client/src/components/DashUsers.jsx
+++ client/src/components/DashUsers.jsx
@@ -52,6 +52,22 @@ export default function DashUsers() {
  
   const handleDeleteUser = async () => {
 
+    try {
+        const res = await fetch(`/api/user/delete/${userIdToDelete}`, {
+          method: 'DELETE',
+        });
+        const data = await res.json();
+    
+        if (res.ok) {
+          setUsers((prev) => prev.filter((user) => user._id !== userIdToDelete));
+          setShowModal(false);
+        } else{
+          console.log(data.message);
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+
   };
 
   return (
@@ -114,7 +130,7 @@ export default function DashUsers() {
             <div className="text-center">
               <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
               <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-                Are you sure you want to delete this post?
+                Are you sure you want to delete this user?
               </h3>
               <div className="flex justify-center gap-4">
                 <Button color="red" onClick={handleDeleteUser}>
