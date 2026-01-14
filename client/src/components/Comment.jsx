0a725ddaa0c31b963b972dc9ef8a9fcc7cc57acf
Lahiru Niroshan <e20272@eng.pdn.ac.lk>
2026-01-14 22:12:52 +0530

    show the comments of a post

---
+++ client/src/components/Comment.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react"
+import moment from "moment";
+
+
+export default function Comment({ comment }) {
+    const [user, setUser] = useState(null);
+    useEffect(() => {
+        const getUser = async () => {
+            try {
+                const res = await fetch(`/api/user/${comment.userId}`);
+                if (res.ok) {
+                    const data = await res.json();
+                    setUser(data);
+                }
+            } catch (error) {
+                console.log(error.message);
+            }
+        }
+        getUser();
+    }, [comment]);
+  return (
+    <div className="flex p-4 border-b dark:border-gray-600 text-sm">
+        <div className="flex-shrink-0 mr-3">
+            <img className="w-10 h-10 rounded-full bg-gray-200" 
+            src={user?.profilePicture} alt={user?.username} />
+        </div>
+        <div className="flex-1">
+            <div className="flex mb-1 items-center">
+                <span className="font-bold mr-1 text-xs truncate">{user? `@${user.username}` : "anonymous user"}</span>
+                <span className="text-green-500 text-xs">
+                    {moment(comment.createdAt).fromNow()}
+                </span>
+            </div>
+            <p className="text-gray-500 mb-2">{comment.content}</p>
+        </div>
+    </div>
+  )
+}
+
